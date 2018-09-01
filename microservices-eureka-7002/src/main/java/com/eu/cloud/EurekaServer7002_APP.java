bde90aa2b366f93f7e24c7f8f745988a54da8906
ZYypl <1178051502@qq.com>
2018-09-01 22:09:33 +0800

    7:简单集群配置

---
+++ microservices-eureka-7002/src/main/java/com/eu/cloud/EurekaServer7002_APP.java
@@ -0,0 +1,17 @@
+package com.eu.cloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer   //服务server
+public class EurekaServer7002_APP {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EurekaServer7002_APP.class, args);
+
+    }
+}
---
+++ microservices-eureka-7003/src/main/java/com/eu/cloud/EurekaServer7003_APP.java
@@ -0,0 +1,17 @@
+package com.eu.cloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer   //服务server
+public class EurekaServer7003_APP {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EurekaServer7003_APP.class, args);
+
+    }
+}
